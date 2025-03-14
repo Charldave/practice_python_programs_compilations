158327f1d03546137a5cd02e09b2abef12a21bd7
Charldave <redito.c17@gmail.com>
2025-03-15 01:53:21 +0800

    print total sum

--- Batch_1/print_sum_of_two_numbers.py
+++ Batch_1/print_sum_of_two_numbers.py
@@ -1,4 +1,8 @@
 #input num_1
+num_1=float(input("Enter the first number: "))
 #input num_2
+num_2=float(input("Enter the second number: "))
 #add num_1+num_2
+result = num_1+num_2
 #print result
+print (f"the sum of the two numbers is {result}")
