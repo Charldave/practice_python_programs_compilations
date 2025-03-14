65080962e09cf2b28be35e6bf331496f83a8b4ad
Charldave <redito.c17@gmail.com>
2025-03-15 06:58:07 +0800

    print not equal

--- Batch_2/print_not_equal.py
+++ Batch_2/print_not_equal.py
@@ -1,3 +1,8 @@
 #set input variables
+num_1=float(input("Enter the first number: "))
+num_2=float(input("Enter the second number: "))
 #if statement suing ==
-#print result
+if num_1==num_2:
+    print("Equal Numbers")#print result
+else:
+    print("Not Equal")
