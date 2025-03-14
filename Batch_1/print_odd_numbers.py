a9f44375a8260713030641a5bd976001210acd32
Charldave <redito.c17@gmail.com>
2025-03-15 05:10:16 +0800

    applying for loop

--- Batch_1/print_odd_numbers.py
+++ Batch_1/print_odd_numbers.py
@@ -1,6 +1,9 @@
 #initialize a variable for odd
+odd_count = 0
 #use for loop
+for count in range(10):
 #set input variable
+    num = int(input(f"Enter number {count+1} : "))
 #if statement with modulus
 #add 1 value in odd variable
 #print result
