b476c9c5c55787d2261f413e04c6c6600deff1a9
Charldave <redito.c17@gmail.com>
2025-03-15 01:12:35 +0800

    print bigger number

--- Batch_1/print_bigger_number.py
+++ Batch_1/print_bigger_number.py
@@ -1,6 +1,6 @@
-#input two numbers
-#check num1>num2
-#print num1
-#elif num1<num2
-#print num2
-#else both are equal
+num_1=float(input("Enter the first number: "))
+num_2=float(input("Enter the second number: "))
+if num_1>num_2:
+    print(f"The bigger number is {num_1}")
+elif num_1<num_2:
+    print(f'the bigger number is {num_2}')
