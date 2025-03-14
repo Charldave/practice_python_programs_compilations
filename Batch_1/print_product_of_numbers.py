2fa0e38935fbc585993e1f6b623382da10cfc5ee
Charldave <redito.c17@gmail.com>
2025-03-15 02:15:04 +0800

    multiplied using "*" operator

--- Batch_1/print_product_of_numbers.py
+++ Batch_1/print_product_of_numbers.py
@@ -1,3 +1,7 @@
 #set variables annd inputs
+num_1=float(input("Enter the first number: "))
+num_2=float(input("Enter the second number: "))
 #apply operator "*"
+product=num_1*num_2
 #print result
+print(f"the product of the two numbers is {product}")
