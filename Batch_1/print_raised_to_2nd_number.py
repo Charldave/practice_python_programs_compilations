50e8eb699df20899f1bc0a2586968908da024c13
Charldave <redito.c17@gmail.com>
2025-03-15 04:38:39 +0800

    fixed a mistake (forgot the snakescase rule)

--- Batch_1/print_raised_to_2nd_number.py
+++ Batch_1/print_raised_to_2nd_number.py
@@ -1,7 +1,7 @@
 #set variables and inputs
-num1=float(input("Enter the first number: "))
-num2=float(input("Enter the second number: "))
+num_1=float(input("Enter the first number: "))
+num_2=float(input("Enter the second number: "))
 #use operator **
-result=num1**num2
+result=num_1**num_2
 #print result
-print(f"the result of {num1} when raised to {num2} is {result}")
+print(f"the result of {num_1} when raised to {num_2} is {result}")
