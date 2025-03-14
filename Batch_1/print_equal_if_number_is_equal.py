d3a29cf6534b6ea216a7bcb46c7ef73dd356e6e4
Charldave <redito.c17@gmail.com>
2025-03-15 01:42:24 +0800

    output

--- Batch_1/print_equal_if_number_is_equal.py
+++ Batch_1/print_equal_if_number_is_equal.py
@@ -1,4 +1,10 @@
 #input num_1
+num_1=float(input("Enter the first number: "))
 #input num_2
+num_2=float(input("Enter the second number: "))
 #check num using ==
+if num_1==num_2:
+    print("Equal")
 #print result
+else:
+    print("Not Equal")
