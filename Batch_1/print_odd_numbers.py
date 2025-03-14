9c1af71a9c2def316fdc0e4f5d666a81b92d09ae
Charldave <redito.c17@gmail.com>
2025-03-15 05:23:45 +0800

    print count of odd numbers

--- Batch_1/print_odd_numbers.py
+++ Batch_1/print_odd_numbers.py
@@ -5,5 +5,8 @@ for count in range(10):
 #set input variable
     num = int(input(f"Enter number {count+1} : "))
 #if statement with modulus
+    if num % 2 != 0:
 #add 1 value in odd variable
+        odd_count += 1
 #print result
+print(f'the amount of odd numbers you entered is {odd_count}')
