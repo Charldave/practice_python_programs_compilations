35c54b19f10713ea8c3c33ccca1aed5175fa8718
Charldave <redito.c17@gmail.com>
2025-03-15 07:27:47 +0800

    print quotient without decimals

--- Batch_2/print_quotient_no_decimal.py
+++ Batch_2/print_quotient_no_decimal.py
@@ -1,5 +1,11 @@
 #set variable for inputs
+num_1=float(input("Enter the first number: "))
+num_2=float(input("Enter the second number: "))
 #set quotient operation
+quotient=num_1/num_2
 #if statement
-#print result
+if quotient!=0:
+    print((f"{num_1} divided by {num_2} is {int(quotient)}"))
 #else statement
+else:
+    print("invalid")
