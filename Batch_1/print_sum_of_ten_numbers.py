3f3ff1af9095440be62bb29157e0ffc8587515a2
Charldave <redito.c17@gmail.com>
2025-03-15 04:35:46 +0800

    print total of all numbers

--- Batch_1/print_sum_of_ten_numbers.py
+++ Batch_1/print_sum_of_ten_numbers.py
@@ -1,6 +1,10 @@
 #set total=0
+total=0
 #use for loop in range 10
-#set variable for inputs
-#ask user to input
-#add input to the total
+for count in range(0, 10):
+    #set variable for inputs/ask user to input
+    number=float(input(f'Enter number {count+1}: '))
+    #add input to the total
+    total+=number
 #print the total
+print(f'the total of all numbers is {total}')
