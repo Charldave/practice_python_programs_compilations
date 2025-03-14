a1d1ab39c83e4debc8af0d20bc79b9e6b656b1fa
Charldave <redito.c17@gmail.com>
2025-03-15 07:11:27 +0800

    using absolute(abs) function

--- Batch_2/print_the_difference.py
+++ Batch_2/print_the_difference.py
@@ -1,3 +1,7 @@
 #set input variables
-#set difference variable=absolute(num_1-num_2
+num_1=float(input("Enter the first number: "))
+num_2=float(input("Enter the second number: "))
+#set difference variable=absolute(for psitive output)
+difference=abs(num_1-num_2)
 #print result
+print(f"the difference are {difference}")
