4a5989f1c10fc5778d1763282cee1e6335f4d9b1
Charldave <redito.c17@gmail.com>
2025-03-15 06:46:32 +0800

    print smaller number

--- Batch_2/print_smaller_number.py
+++ Batch_2/print_smaller_number.py
@@ -1,3 +1,8 @@
 #set variable for inputs
-#if elif statement > and <
-#print result
+num_1=float(input("Enter the first number: "))
+num_2=float(input("Enter the second number: "))
+#if elif statement > and <, print result
+if num_1>num_2:
+    print(f'{num_2} is smaller')
+elif num_1<num_2:
+    print(f'{num_1} is smaller')
