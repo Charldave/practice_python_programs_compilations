401b3926acb866c60af16a873021b118bd26642d
Charldave <redito.c17@gmail.com>
2025-03-15 04:26:27 +0800

    fixed wrong sequence

--- Batch_1/print_sum_of_ten_numbers.py
+++ Batch_1/print_sum_of_ten_numbers.py
@@ -1,5 +1,6 @@
 #set total=0
 #use for loop in range 10
 #set variable for inputs
-#add the input to the total
-#ask user to input 10 numbers
+#ask user to input
+#add input to the total
+#print the total
