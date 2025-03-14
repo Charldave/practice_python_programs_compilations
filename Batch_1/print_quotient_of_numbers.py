ff61312b7545d4bb03259e1654748f465276c76c
Charldave <redito.c17@gmail.com>
2025-03-15 03:09:38 +0800

    changed the operator to "==" for both number to fix error inputs

--- Batch_1/print_quotient_of_numbers.py
+++ Batch_1/print_quotient_of_numbers.py
@@ -1,8 +1,10 @@
 #set variables and inputs
 num_1=float(input("Enter the first number: "))
 num_2=float(input("Enter the second number: "))
-#use if statements with "!=" operator to reject 0
-if num_1!=0:
+#use if statements with "" operator to reject 0
+if num_1==0 or num_2==0:
+    print(' Inavlid input "0"')
+else:
 #divide the inputs using "/"
     quotient=num_1/num_2
 #print result
