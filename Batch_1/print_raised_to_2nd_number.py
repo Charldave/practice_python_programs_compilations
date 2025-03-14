d9ef8b8443c10c18c7d9164fa3bcfbc13dee98e5
Charldave <redito.c17@gmail.com>
2025-03-15 03:21:56 +0800

    print raised to 2nd num

--- Batch_1/print_raised_to_2nd_number.py
+++ Batch_1/print_raised_to_2nd_number.py
@@ -1,3 +1,7 @@
 #set variables and inputs
+num1=float(input("Enter the first number: "))
+num2=float(input("Enter the second number: "))
 #use operator **
+result=num1**num2
 #print result
+print(f"the result of {num1} when raised to {num2} is {result}")
