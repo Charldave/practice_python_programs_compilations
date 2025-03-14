cb9abb6b9c7b36bd3fe0144fdafd2e79b185946f
Charldave <redito.c17@gmail.com>
2025-03-15 01:34:10 +0800

    pseudocode

---
+++ Batch_1/print_equal_if_number_is_equal.py
@@ -0,0 +1,4 @@
+#input num_1
+#input num_2
+#check num using ==
+#print result
---
+++ Batch_1/print_equal_if_number_is_equal.py.py
@@ -0,0 +1,6 @@
+num_1=float(input("Enter the first number: "))
+num_2=float(input("Enter the second number: "))
+if num_1==num_2:
+    print("Equal")
+else:
+    print("Not Equal")
