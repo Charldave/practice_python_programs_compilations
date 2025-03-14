08c2365cf1fe3aa30e61f2e4980419e4885b9a26
Charldave <redito.c17@gmail.com>
2025-03-15 07:37:35 +0800

    minor adjustments for "0" inputs

--- Batch_2/print_quotient_no_decimal.py
+++ Batch_2/print_quotient_no_decimal.py
@@ -1,11 +1,11 @@
 #set variable for inputs
 num_1=float(input("Enter the first number: "))
 num_2=float(input("Enter the second number: "))
-#set quotient operation
-quotient=num_1/num_2
 #if statement
-if quotient!=0:
-    print((f"{num_1} divided by {num_2} is {int(quotient)}"))
+if num_2!=0:
+    #set quotient operation
+    quotient=num_1/num_2
+    print((f"{int(num_1)} divided by {int(num_2)} is {int(quotient)}"))
 #else statement
 else:
     print("invalid")
