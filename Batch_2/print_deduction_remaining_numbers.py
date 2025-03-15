099d7b3f231c41ae541dde6fe2c462e9f2832927
Charldave <redito.c17@gmail.com>
2025-03-15 14:04:26 +0800

    using append() method for inputs

--- Batch_2/print_deduction_remaining_numbers.py
+++ Batch_2/print_deduction_remaining_numbers.py
@@ -1,5 +1,9 @@
 #create empty list
-#for loop user input 0-10
+list=[]
+#for loop range to 10
+for count in range (0, 10):
+    num=float(input(f'Enter number {count+1}: '))#input
 #append input to list
+    list.append(num)
 #subtract input
 #print result
