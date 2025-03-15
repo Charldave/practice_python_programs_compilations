586131e78dbb7d50a198ab9e25214044b8795773
Charldave <redito.c17@gmail.com>
2025-03-15 08:19:03 +0800

    adjusted, rejects division by 0

--- Batch_2/print_the_remainder.py
+++ Batch_2/print_the_remainder.py
@@ -1,14 +1,15 @@
 #set variable inputs
 num_1=float(input(f"Enter 1st num: "))
 num_2=float(input(f"Enter 2nd num: "))
-#check remainder using modulus %
-remainder=num_1%num_2
-#if statements !=0, print remainder
-if remainder !=0:
-    print(f"The remainder of {num_1} over {num_2} is {remainder}")
-#elif print no remainder
-elif remainder==0:
-    print("no remainder")
-#for "0" inputs
+
+if num_2==0:
+    print('invalid input, cannot be divided by zero')
 else:
-    print("invalid")
+#check remainder using modulus %
+    remainder=num_1%num_2
+#if statements !=0, print remainder
+    if remainder !=0:
+        print(f"The remainder of {num_1} over {num_2} is {remainder}")
+#elif print no remainder
+    else:
+        print("no remainder")
