27ee3699493987a4b5fa99e78ceb73748f2212fb
Charldave <redito.c17@gmail.com>
2025-03-15 18:09:14 +0800

    shows all duplicated numbers

--- batch_3/print_display_duplicate.py
+++ batch_3/print_display_duplicate.py
@@ -1,9 +1,17 @@
 #set list[]
+list=[]
 #for loop range 10
+for count in range (0, 10):
     #input variabes, ask user
+    num=float(input(f'Enter number {count+1}:'))
     #append(input)
+    list.append(num)
 #duplicate list
+duplicated=[]
 #for num in list
+for num in list:
     #if .count num>1
-        #duplicate.append
+    if list.count(num)>1:
+        duplicated.append(num)
 #print result
+print(f'duplicate numbers {duplicated}')
