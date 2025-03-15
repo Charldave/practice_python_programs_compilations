b06e74ee8a478ca26d3e1f3585849d455eeeef68
Charldave <redito.c17@gmail.com>
2025-03-15 21:29:25 +0800

    using .sort() function

--- batch_3/print_highest_to_lowest.py
+++ batch_3/print_highest_to_lowest.py
@@ -1,8 +1,12 @@
 #create a list
-#while true loop
-    #ask user input
-        #append inputs
-    #except valueerror
-        #break
+list=[]
+while True:
+    try:
+        num=float(input('Enter number: '))
+        list.append(num)#append inputs
+    except ValueError:
+        print('Invalid input, proceeding to sort')
+        break
 #.sort()
-#print result
+list.sort()
+print(list)
