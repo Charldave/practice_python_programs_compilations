3b48417cca89709e065dd778174b48b1a9203106
Charldave <redito.c17@gmail.com>
2025-03-15 22:17:04 +0800

    displays numbers that have duplicates

--- batch_4/print_number_with_duplicate.py
+++ batch_4/print_number_with_duplicate.py
@@ -1,9 +1,13 @@
 #set list[]
- #for loop range 10
-     #input variabes, ask user
-     #append(input)
- #duplicate list
- #for num in list
-     #if .count num>1
+list=[]
+for count in range (0, 10):
+    num=float(input(f'Enter number {count+1}: '))#input
+    list.append(num)#append(input)
+#duplicate list
+duplicates=[]
+
+for num in list:
+    if list.count(num) > 1 and num not in duplicates:
          #duplicate.append
- #print result
+        duplicates.append(num)
+print(duplicates)#output
