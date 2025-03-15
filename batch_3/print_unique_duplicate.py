df2d1438f8f1828404fb48b36ca27c8c5e036780
Charldave <redito.c17@gmail.com>
2025-03-15 19:19:40 +0800

    user input in while loop

--- batch_3/print_unique_duplicate.py
+++ batch_3/print_unique_duplicate.py
@@ -1,16 +1,18 @@
-#create list 
+#create list
+list=[]
 #while True loop
-    #try
-        #display enter num
-        #ask user input
-
-        #if input not a number
-            #display inavalid
-            #break
+while True:
+    try:
+        user_num=input('Enter a number: ')#ask user input
+        
+        if user_num == "" or not user_num.isdigit():#if input not a number
+            print('Please Enter a valid number')#display invalid
+            break
         #convert to int
-
         #if in list display duplicate
         #else display unique
         #list.append()
     #except valueerror
-        #break
+    except ValueError:
+        print("invalid inputs please try again")
+        break
