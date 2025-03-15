02a317d62524b6c268f492850c1c599824ebc0f0
Charldave <redito.c17@gmail.com>
2025-03-16 04:32:37 +0800

    prints highest to lowest

---
+++ batch_4/print_highest_to_lowest.py
@@ -0,0 +1,12 @@
+#basically the same as batch_3/print_lowest_to_highest.py with reverse sort
+list=[]
+while True:
+    try:
+        num=float(input('Enter number: '))
+        list.append(num)#append inputs
+    except ValueError:
+        print('Invalid input, proceeding to sort')
+        break
+#.sort()
+list.sort(reverse=True)#add reverse=True to reverse the outputs
+print(list)
