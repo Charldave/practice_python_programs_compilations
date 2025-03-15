b59410632e343a740427a0e32f1920e23e58c55f
Charldave <redito.c17@gmail.com>
2025-03-15 15:40:38 +0800

    applied modulus 5

--- Batch_2/print_except_end_five_and_zero.py
+++ Batch_2/print_except_end_five_and_zero.py
@@ -1,4 +1,8 @@
 #use for loop rang 100
+for count in range (0, 100):
 #if %5!=0
-    #print result
-#count
+    if count%5!=0:
+        #print result
+        print(count)
+    #count+1
+    count+=1
