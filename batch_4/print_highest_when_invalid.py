66e20c69458528dd50231c332d68c679b6d96e3b
Charldave <redito.c17@gmail.com>
2025-03-16 04:09:17 +0800

    while true loop

--- batch_4/print_highest_when_invalid.py
+++ batch_4/print_highest_when_invalid.py
@@ -1,8 +1,13 @@
 #set variable to None
+highest_num=None
 #start while true loop
-    #display and ask input
+while True:
+    try:
+        num=float(input('Enter number: '))
     #set if is None or > condition
-        #update variable value
-    #except
-        #break
+        if highest_num is None or num>highest_num:
+            highest_num=num#update variable value
+    except ValueError:
+        print('invalid')
+        break
 #print result
