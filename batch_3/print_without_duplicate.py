0c80ab136e929b5171f11c3968afcf9698f3f7d1
Charldave <redito.c17@gmail.com>
2025-03-15 17:18:31 +0800

    append user inputs

--- batch_3/print_without_duplicate.py
+++ batch_3/print_without_duplicate.py
@@ -1,7 +1,14 @@
 #set empty list
+list=[]
 #ask user for inputs, for loop range 10
+for count in range(0, 10):
+    num=float(input(f'Enter number {count+1}: '))
     #.append(inputs)
+    list.append(num)
 #no duplicates list
 #for loop in list
     #.count(num) 
-#print reseult
+#print result
+
+
+
