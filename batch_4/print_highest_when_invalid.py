31e27950acc3190d0608e51145b3cdfd79bf2f59
Charldave <redito.c17@gmail.com>
2025-03-16 04:03:10 +0800

    pseudocode

---
+++ batch_4/print_highest_when_invalid.py
@@ -0,0 +1,8 @@
+#set variable to None
+#start while true loop
+    #display and ask input
+    #set if is None or > condition
+        #update variable value
+    #except
+        #break
+#print result
