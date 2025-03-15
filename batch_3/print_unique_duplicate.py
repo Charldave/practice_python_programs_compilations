e533c52a0600d105c2188b9e1c4ebab209b25465
Charldave <redito.c17@gmail.com>
2025-03-15 19:11:41 +0800

    pseudocode

---
+++ batch_3/print_unique_duplicate.py
@@ -0,0 +1,16 @@
+#create list 
+#while True loop
+    #try
+        #display enter num
+        #ask user input
+
+        #if input not a number
+            #display inavalid
+            #break
+        #convert to int
+
+        #if in list display duplicate
+        #else display unique
+        #list.append()
+    #except valueerror
+        #break
