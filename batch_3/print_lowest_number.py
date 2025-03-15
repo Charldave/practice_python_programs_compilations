8e0e0bffc976300523548d128396b3137f63d368
Charldave <redito.c17@gmail.com>
2025-03-15 20:01:53 +0800

    pseudocode

---
+++ batch_3/print_lowest_number.py
@@ -0,0 +1,15 @@
+#create list 
+#while True loop
+    #try
+        #display enter num
+        #ask user input
+
+        #if list is none or input<list
+            #lowest=input
+
+    #except ValueError
+        #display invalid
+        #break
+
+        #if list not None display list
+        #else display no lowest num
