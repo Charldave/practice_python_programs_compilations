d701e84d1b84d9e3e948c4ff064484d239183dbf
Charldave <redito.c17@gmail.com>
2025-03-16 03:34:17 +0800

    used a dictionary initialization instead

--- batch_4/print_most_duplicated.py
+++ batch_4/print_most_duplicated.py
@@ -9,9 +9,15 @@ while True:
     except ValueError:
         print('Inavlid number input ')
         break
-if list:
-    most_dupli
-#use set func in list
-    #check frequent num in set
-#save highest count
+count_dict = {}#used a empty dictionary instead 
+for num in list:
+    count_dict[num] = count_dict.get(num, 0) + 1
+#finds the number with biggest count
+most_dupli = None
+max_count = 0
+for num, count in count_dict.items():
+    if count > max_count:
+        max_count = count
+        most_dupli = num
 #display result
+print(f"The number with the most duplicates is: {most_dupli}")
