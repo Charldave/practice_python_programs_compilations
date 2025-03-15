4f875153b134e9013b53d1196855656c422c8936
Charldave <redito.c17@gmail.com>
2025-03-15 17:46:48 +0800

    used a .count() method

--- batch_3/print_without_duplicate.py
+++ batch_3/print_without_duplicate.py
@@ -6,9 +6,11 @@ for count in range(0, 10):
     #.append(inputs)
     list.append(num)
 #no duplicates list
+no_dupli=[]
 #for loop in list
+for num in list:
     #.count(num) 
+    if list.count(num) ==1:
+        no_dupli.append(num)
 #print result
-
-
-
+print(f'Non-duplicated numbers are {no_dupli}.')
