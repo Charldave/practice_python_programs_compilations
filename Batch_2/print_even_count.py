7633129daed956c37482a97e76d4217e7acf678d
Charldave <redito.c17@gmail.com>
2025-03-15 14:46:43 +0800

    added modulus if statement %2==0

--- Batch_2/print_even_count.py
+++ Batch_2/print_even_count.py
@@ -4,5 +4,8 @@ even_count=0
 for count in range (0, 10):
     num=float(input(f'Enter number {count+1}: '))#ask inputs
 #if %2==0
+    if num%2 == 0:
 #even count+=1
+        even_count+=1
 #print even count
+print(f'Even count is {even_count}')
