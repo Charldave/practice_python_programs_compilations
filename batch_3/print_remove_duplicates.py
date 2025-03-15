fa29c12cdb49520af561fbd2b39d030b5170c8ca
Charldave <redito.c17@gmail.com>
2025-03-15 22:39:06 +0800

    fixed wrong output

--- batch_3/print_remove_duplicates.py
+++ batch_3/print_remove_duplicates.py
@@ -3,15 +3,14 @@ list=[]
 #for loop range 10
 for count in range (0, 10):
     #input variabes, ask user
-    num=float(input(f'Enter number {count+1}:'))
+    num=float(input(f'Enter number {count+1}: '))
     #append(input)
     list.append(num)
-#duplicate list
-duplicated=[]
-#for num in list
+#remove dupli list
+removed_dupli=[]
+
+print("no duplicates: ")
 for num in list:
-    #if .count num>1
-    if list.count(num) > 1 and num not in duplicated:
-        duplicated.append(num)
-#print result
-print(f'duplicate numbers {duplicated}')
+    if num not in removed_dupli:
+        print(num)
+        removed_dupli.append(num)#to record duplicates
