1c8a08eb7eb1ad6ee21547e0ee1782c2a9494eb6
Charldave <redito.c17@gmail.com>
2025-03-15 23:21:29 +0800

    pseudocode

---
+++ batch_4/print_most_duplicated.py
@@ -0,0 +1,11 @@
+#create list
+#while loop inputs
+    #try
+    #append
+    #except
+#no int input display no valid number
+
+#use set func in list
+    #check frequent num in set
+#save highest count
+#display result
