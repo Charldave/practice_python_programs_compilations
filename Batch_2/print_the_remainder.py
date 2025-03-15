99606b6ac45e50c5935790e9c51adce62068e3a3
Charldave <redito.c17@gmail.com>
2025-03-15 08:07:52 +0800

    find remainder using modulus

--- Batch_2/print_the_remainder.py
+++ Batch_2/print_the_remainder.py
@@ -1,5 +1,14 @@
 #set variable inputs
+num_1=float(input(f"Enter 1st num: "))
+num_2=float(input(f"Enter 2nd num: "))
 #check remainder using modulus %
-#if statements !=0
-#print remainder
-#else print no remainder
+remainder=num_1%num_2
+#if statements !=0, print remainder
+if remainder !=0:
+    print(f"The remainder of {num_1} over {num_2} is {remainder}")
+#elif print no remainder
+elif remainder==0:
+    print("no remainder")
+#for "0" inputs
+else:
+    print("invalid")
