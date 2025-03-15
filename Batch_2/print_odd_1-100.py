3139050bec1e10b2a224e36a1dc9f915f49192da
Charldave <redito.c17@gmail.com>
2025-03-15 15:13:54 +0800

    adjusted to removed output "101"

--- Batch_2/print_odd_1-100.py
+++ Batch_2/print_odd_1-100.py
@@ -2,8 +2,8 @@
 odd_numbers=0
 #use while loop <=100
 while odd_numbers<=100:
-#odd number +1
-    odd_numbers+=1
 #if %2!=0
     if odd_numbers%2!=0:
         print(odd_numbers)
+    #odd number +1
+    odd_numbers+=1
