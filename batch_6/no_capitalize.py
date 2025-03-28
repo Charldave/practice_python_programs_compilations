367171e1c1c35c39167050f419f8c9000d283ddd
Charldave <redito.c17@gmail.com>
2025-03-29 00:17:05 +0800

    pseudocode

---
+++ batch_6/no_capitalize.py
@@ -0,0 +1,8 @@
+#ask user input
+#check if input is not empty
+    #capitalize first letter using upper()
+    #convert remaining to lowercase using lower()
+    #combine and save to capped
+#else
+    #print "empty input"
+#print capped
