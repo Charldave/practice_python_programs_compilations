1553405b6072ff76a0bdc55d9329ffb2340ed69c
Charldave <redito.c17@gmail.com>
2025-03-29 00:24:03 +0800

    pseudocode

---
+++ batch_6/no_title.py
@@ -0,0 +1,8 @@
+#ask user input
+#split the input into words
+#initialize a list for capitalized words
+#for each word in words
+    #capitalize first letter and lowercase the rest
+    #append to the list
+#join words into a string
+#print the result
