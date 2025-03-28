1323ffa613f704bddb4dd1ac0a3d2d3274882bb3
Charldave <redito.c17@gmail.com>
2025-03-29 00:13:15 +0800

    pseudocode

---
+++ batch_6/no_swapcase.py
@@ -0,0 +1,10 @@
+#ask user input
+#initialize empty swapped
+#for each case in input
+    #if case is uppercase
+        #convert to lowercase and add to swapped
+    #else if case is lowercase
+        #convert to uppercase and add to swapped
+    #else
+        #add case to swapped
+#print swapped
