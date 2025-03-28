da7b3cd91562587f24e35de3f8f013721332b377
Charldave <redito.c17@gmail.com>
2025-03-29 00:07:34 +0800

    no center() func

--- batch_6/no_center.py
+++ batch_6/no_center.py
@@ -1,8 +1,11 @@
-#get input
-  #input desired width
-  #if string length >= desired width
-    #print string
-  #else
-    #calculate extra spaces
-    #divide spaces for left and right
-    #print spaces + text + spaces
+text = input("Enter a string: ")
+desired_width = int(input("Enter the total width: "))
+
+if len(text) >= desired_width:
+    print(text)
+else:
+    extra_spaces = desired_width - len(text)
+    spaces_before = extra_spaces // 2
+    spaces_after = extra_spaces - spaces_before
+
+    print(' ' * spaces_before + text + ' ' * spaces_after)
