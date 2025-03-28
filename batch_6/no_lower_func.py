3e25edaa62d0058ce60758f9f7aba3228accf7e8
Charldave <redito.c17@gmail.com>
2025-03-28 23:21:27 +0800

    no lower()

--- batch_6/no_lower_func.py
+++ batch_6/no_lower_func.py
@@ -1,8 +1,11 @@
-#ask user input
-#lowered = empty string
-#for loop i in input
-#   if 'A' <= i <= 'Z'
-#       lowered += chr(ord(i) + 32)
-#   else
-#       lowered += i
-#print lowered
+user_inputs = input("Enter strings: ") 
+
+lowered = ''
+
+for c in user_inputs:
+    if 'A' <= c <= 'Z':
+        lowered += chr(ord(c) + 32)
+    else:
+        lowered += c
+
+print(lowered)
