51a7a949a3fd788c765fe7cbee0a24324217cec6
Charldave <redito.c17@gmail.com>
2025-03-29 00:06:12 +0800

    pseudocode

---
+++ batch_6/no_center.py
@@ -0,0 +1,8 @@
+#get input
+  #input desired width
+  #if string length >= desired width
+    #print string
+  #else
+    #calculate extra spaces
+    #divide spaces for left and right
+    #print spaces + text + spaces
