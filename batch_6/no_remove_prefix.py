4fe2145a40764c8723a7d87fb57a7584ed4bf550
Charldave <redito.c17@gmail.com>
2025-03-28 23:00:33 +0800

    pseudocode

---
+++ batch_6/no_remove_prefix.py
@@ -0,0 +1,8 @@
+#ask user for input
+#set index to 0
+#sor each character in input
+#   if character is the same as the first character
+#       increase index by 1
+#   else
+#       break
+#print the substring starting from the index
