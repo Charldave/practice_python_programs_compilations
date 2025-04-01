c43bea182827f27ac2607571924ff4a416dc6e63
Charldave <redito.c17@gmail.com>
2025-04-01 23:29:11 +0800

    pseudocode

---
+++ batch_7/no_upper.py
@@ -0,0 +1,11 @@
+#ask user input
+
+#dictionary for lowercase to uppercase(a:A-z:Z)
+
+#initialize empty string
+
+#loop each character in user_input
+    #if character lower, convert uppercase add uppercase
+    #if not lower, add it uppercase
+
+#print uppercase
