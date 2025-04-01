06859ecd61a74a438ffaa28a5e1a13296454783c
Charldave <redito.c17@gmail.com>
2025-04-02 00:47:06 +0800

    pseudocode

---
+++ batch_7/no_index.py
@@ -0,0 +1,14 @@
+#ask user input
+#ask user character to find
+
+#found_index = -1
+
+#for count in len(user input)
+    #if user input == character to find
+        #found_index = count
+        #break
+
+#if found_index != -1
+    #print found_index
+#else
+    #print "not found"
