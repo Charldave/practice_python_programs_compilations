99c120f78967b83fa6e7c62186e7e993074e1fb6
Charldave <redito.c17@gmail.com>
2025-04-01 23:38:26 +0800

    pseudocode

---
+++ batch_7/no_lower.py
@@ -0,0 +1,11 @@
+#ask user input  
+
+#dictionary for uppercase to lowercase (A:a-Z:z)  
+
+#initialize empty string  
+
+#loop each character in user input  
+    #if character upper, convert lowercase add to lowercase 
+    #if not upper, add it lowercase
+
+#print lowercase
