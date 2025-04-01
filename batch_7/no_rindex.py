8c13810020009f9ead88bc2ddcde459fb7e82ccf
Charldave <redito.c17@gmail.com>
2025-04-02 01:14:14 +0800

    just added a "-1, -1, -1" in no_index.py to reverse the count for .rindex output

---
+++ batch_7/no_rindex.py
@@ -0,0 +1,14 @@
+user_input = input("Enter a string: ")
+string_find = input("Enter the character to find: ")
+
+found_index = -1
+
+for count in range(len(user_input)-1, -1, -1):
+    if user_input[count] == string_find:
+        found_index = count
+        break
+
+if found_index != -1:
+    print(found_index)
+else:
+    print("not found")
