f99276630524a36adc551c37a0128b1b110ac4e7
Charldave <redito.c17@gmail.com>
2025-04-02 00:01:23 +0800

    no .rjust

--- batch_7/no_rjust.py
+++ batch_7/no_rjust.py
@@ -1,10 +1,9 @@
-#ask user input
+user_input = input("Enter a string: ")
+desired_width = int(input("Enter the total width: "))
 
-#ask desired width
+spaces_to_add = desired_width - len(user_input)
 
-#calculate spaces
-
-#if input smaller than width
-    #add spaces at the beginning
-    #print the result
-#else, print the input
+if spaces_to_add > 0:
+    print(' ' * spaces_to_add + user_input)
+else:
+    print(user_input)
