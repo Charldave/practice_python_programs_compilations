5b935535cb4a2199ef74b959b1e1276b5ba839ad
Charldave <redito.c17@gmail.com>
2025-04-15 20:38:05 +0800

    creates file storage using "open()"

--- file_handling/quiz_creator.py
+++ file_handling/quiz_creator.py
@@ -1,15 +1,1 @@
-#open file/create file
-#while true loop:
-    #ask for question until user types "exit"
-    #If "exit": 
-        #break
-
-    #set a = input()
-    #set b = input()
-    #set c = input()
-    #set d = input()
-    #set correct answer = input()
-    
-    #write input question to file
-    #write input(a, b, c, d)
-    #Write input answer
+file_1 = open('file_1.txt', 'w')
