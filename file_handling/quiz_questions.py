cd7cd803e772ae40644f84718a666fb0b7c4140a
Charldave <redito.c17@gmail.com>
2025-05-09 21:27:15 +0800

    imports random and use of for loops

--- file_handling/quiz_questions.py
+++ file_handling/quiz_questions.py
@@ -1,11 +1,12 @@
-#with open file
-#read lines
-#group each question and choices to block
-#store blocks in list
-#random.(question block)
-#for loop block:
-    #print question and choices
-    #user input
-    #check correct answer
-    #print "Correct" or "Wrong"
-#End
+import random
+
+with open('file_1.txt', 'r') as file_1:
+    lines = file_1.readlines()
+
+question_list = []
+for i in range(0, len(lines), 6):
+    question = lines[i:i+6]
+    if len(question) == 6:
+        question_list.append(question)
+
+random.shuffle(question_list)
