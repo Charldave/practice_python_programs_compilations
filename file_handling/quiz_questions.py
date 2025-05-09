a2ba3d6b13b7b7185822507969877432cbce0880
Charldave <redito.c17@gmail.com>
2025-05-09 21:03:03 +0800

    pseudocode

---
+++ file_handling/quiz_questions.py
@@ -0,0 +1,11 @@
+#with open file
+#read lines
+#group each question and choices to block
+#store blocks in list
+#random.(question block)
+#for loop block:
+    #print question and choices
+    #user input
+    #check correct answer
+    #print "Correct" or "Wrong"
+#End
